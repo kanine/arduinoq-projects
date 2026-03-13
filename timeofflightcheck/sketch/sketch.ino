fc65244c8c997cc9db5d75c72e150e160beed9e8
kanine <kaninescripts@gmail.com>
2026-03-13 20:08:11 +1100

    working

--- timeofflightcheck/sketch/sketch.ino
+++ timeofflightcheck/sketch/sketch.ino
@@ -1,25 +1,43 @@
 #include "Arduino_RouterBridge.h"
-#include "Wire.h"
-#include "Adafruit_VL53L1X.h"
+#include <Wire.h>
+#include <VL53L1X.h>
 
-Adafruit_VL53L1X vl53;
+VL53L1X sensor;
+int16_t lastDistance = -1;
+bool sensorReady = false;
+
+int get_distance() {
+  return lastDistance;
+}
 
 void setup() {
   Bridge.begin();
-  Wire.begin();
-  vl53.begin(0x29, &Wire);
-  vl53.startRanging();
-  vl53.setTimingBudget(50);
   Bridge.provide("get_distance", get_distance);
+  Wire1.begin();
+  Wire1.setClock(400000);
 }
 
-void loop() {}
-
-int get_distance() {
-  if (vl53.dataReady()) {
-    int16_t d = vl53.distance();
-    vl53.clearInterrupt();
-    return d;
+void loop() {
+  if (!sensorReady) {
+    Wire1.beginTransmission(0x29);
+    if (Wire1.endTransmission() != 0) {
+      delay(500);
+      return;
+    }
+    sensor.setBus(&Wire1);
+    sensor.setTimeout(200);
+    if (!sensor.init()) {
+      delay(500);
+      return;
+    }
+    sensor.setDistanceMode(VL53L1X::Long);
+    sensor.setMeasurementTimingBudget(50000);
+    sensor.startContinuous(50);
+    sensorReady = true;
   }
-  return -1;
+
+  if (sensor.dataReady()) {
+    lastDistance = sensor.read(false);
+  }
+  delay(10);
 }
